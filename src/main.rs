8953efb05b8bab88044ce39917052e025f471758
Jukes <dev.junqueira@gmail.com>
2023-09-01 21:11:20 -0300

    Added new buttons functionalities: Added About Me Added Social Media Added "back" button so the user can back to the home button page

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use teloxide::{
     prelude::*,
     types::{
         InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputMessageContent,
-        InputMessageContentText, Me, InputFile
+        InputMessageContentText, Me, InputFile, ParseMode
     },
     utils::command::BotCommands,
 };
@@ -17,6 +17,7 @@ enum Command {
     #[command(description = "Show the main menu")]
     Start,
 }
+// Can i create a struct or constant to store all strings and texts so i can edit them easily? Make the bot easy to share
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -66,6 +67,36 @@ fn home_keyboard() -> InlineKeyboardMarkup {
     InlineKeyboardMarkup::new(keyboard)
 }
 
+async fn social_media_buttons() -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+
+    let github_url = Url::parse("https://github.com/junque1r4").unwrap();
+    let linkedin_url = Url::parse("https://www.linkedin.com/in/joao-victor-junqueira-1b9114164/").unwrap();
+    let x_url = Url::parse("https://twitter.com/KRNJun").unwrap();
+
+
+    keyboard.push(vec![InlineKeyboardButton::url(
+        "Github".to_owned(),
+        github_url,
+    )]);
+
+    keyboard.push(vec![InlineKeyboardButton::url(
+        "Linkedin".to_owned(),
+        linkedin_url,
+    )]);
+
+    keyboard.push(vec![InlineKeyboardButton::url(
+        "X".to_owned(),
+        x_url,
+    )]);
+
+    let back_button = InlineKeyboardButton::callback("back", "back");
+
+    keyboard.push(vec![back_button]);
+
+    InlineKeyboardMarkup::new(keyboard)
+}
+
 async fn message_handler(bot: Bot, msg: Message, me: Me, ) -> Result<(), Box<dyn Error + Send + Sync>> {
     if let Some(text) = msg.text() {
         match BotCommands::parse(text, me.username()) {
@@ -73,13 +104,9 @@ async fn message_handler(bot: Bot, msg: Message, me: Me, ) -> Result<(), Box<dyn
                 bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
             }
             Ok(Command::Start) => {                
-                // TODO: Alterar de photo para message, assim consigo alterar o texto no callback_handler.
-                // TODO: Adicionar string invisivel par hyperlink da imagem "ㅤ"
-                let photo_link = Url::parse("https://www.jokesforfunny.com/wp-content/uploads/2021/06/0596bdb89b60fe771acd2f5972a9d3e3-1158x1536.jpg")?;
-                bot.send_photo(msg.chat.id, InputFile::url(photo_link))
-                    .caption("My Portifolio!")
+                bot.send_message(msg.chat.id, "Welcome to my portifolio [ㅤ](https://www.jokesforfunny.com/wp-content/uploads/2021/06/0596bdb89b60fe771acd2f5972a9d3e3-1158x1536.jpg)")
+                    .parse_mode(ParseMode::Markdown)
                     .reply_markup(home_keyboard())
-                    .send()
                     .await?;
                 }
 
@@ -97,7 +124,8 @@ async fn inline_query_handler(bot: Bot, q: InlineQuery, ) -> Result<(), Box<dyn 
     let choose_home_option = InlineQueryResultArticle::new(
         "0",
         "What info do you need?",
-        InputMessageContent::Text(InputMessageContentText::new("Home options:")),
+        InputMessageContent::Text(InputMessageContentText::new("Welcome to my portifolio [ㅤ](https://www.jokesforfunny.com/wp-content/uploads/2021/06/0596bdb89b60fe771acd2f5972a9d3e3-1158x1536.jpg)").parse_mode(ParseMode::Markdown)),
+        
     )
     .reply_markup(home_keyboard());
 
@@ -106,15 +134,52 @@ async fn inline_query_handler(bot: Bot, q: InlineQuery, ) -> Result<(), Box<dyn 
     Ok(())
 }
 
+async fn back_button() -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+    let back_button = InlineKeyboardButton::callback("back", "back");
+
+    keyboard.push(vec![back_button]);
+    InlineKeyboardMarkup::new(keyboard)
+}
+
 async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn Error + Send + Sync>> {
     if let Some(option) = q.data {
         let text = format!("You chose: {option}");
         bot.answer_callback_query(q.id).await?;
 
+        // These giant strings are kinda ugly, 
+        // TODO: Improve string formatting and store in a struct or constant
         if let Some(Message { id, chat, .. }) = q.message {
             match option.to_lowercase().as_str() {
                 "about me" => {
-                    bot.send_message(chat.id, "I'm a 24 years old developer from Brazil. I'm currently working at AleloBrasil.").await?;
+                    let about_me = "
+[ㅤ](https://www.jokesforfunny.com/wp-content/uploads/2021/06/0596bdb89b60fe771acd2f5972a9d3e3-1158x1536.jpg)	
+I'm a 24 years old Cybersecurity Analyst from Brazil. I'm currently working at [Alelo](https://www.alelo.com.br)
+
+I don't have a degree because i quit in the last semester of my Computer Science course, because i was planning to move to another country. ( Political and Economic reasons )
+
+I'm currently studying backend development in Rust! This bot was implemented using the [Teloxide](https://github.com/teloxide/teloxide) library and Rust!
+
+If you want to know more about my skills you can select \"Skills\" in the main menu.";
+                    bot.edit_message_text(chat.id, id, about_me)
+                    .parse_mode(ParseMode::Markdown)
+                    .reply_markup( back_button().await ) // back button
+                    .await?;
+                },
+                "back" => {
+                    bot.edit_message_text(chat.id, id, "Welcome to my portifolio [ㅤ](https://www.jokesforfunny.com/wp-content/uploads/2021/06/0596bdb89b60fe771acd2f5972a9d3e3-1158x1536.jpg)")
+                        .parse_mode(ParseMode::Markdown)
+                        .reply_markup(home_keyboard())
+                        .await?;
+                },
+                "social media" => {
+                    bot.edit_message_text(chat.id, id, "
+Social Media!
+
+Follow me on X, i'm trying to post some things! 🤝")
+                        .parse_mode(ParseMode::Markdown)
+                        .reply_markup(social_media_buttons().await)
+                        .await?;
                 },
                 _ => {
                     bot.send_message(chat.id, text).await?;
